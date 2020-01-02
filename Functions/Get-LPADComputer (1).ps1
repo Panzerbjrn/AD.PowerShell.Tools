8201efef3f9dd0d324c6ef64b11419379f176b10
Lars.Petersson <Lars.Petersson@ihsmarkit.com>
2020-01-02 13:43:09 +0000

    small update

---
+++ Functions/Get-LPADComputer (1).ps1
@@ -0,0 +1,31 @@
+Function Get-LPADComputer
+{
+	[CmdletBinding()]
+	Param(
+		[parameter(Mandatory=$true,ValueFrompipeline=$True)][Alias("Host","CN","MachineName","ComputerName")][String]$Computer
+	)
+	IF (Get-ADComputer -Filter {Name -like $Computer})
+	{
+		$ADComp = Get-ADComputer -Filter {Name -like $Computer} -Properties *
+	}
+	ELSE
+	{
+		Write-Host "Nothing Found"
+	}
+	
+	IF (($ADComp|measure).Count -GT 1)
+		{
+		$Menu = @{}
+		for ($i=1;$i -le $ADComp.count; $i++) {
+		Write-Host "$i. $($ADComp[$i-1].Name), $($ADComp[$i-1].DNSHostName)"
+		$Menu.Add($i,($ADComp[$i-1].Name))
+		}
+		[int]$ans = Read-Host 'Enter Selection'
+		$Selection = $Menu.Item($ans)
+		$ADComp = $Selection
+		Get-ADComputer -Filter {Name -like $ADComp} -Properties * | Select Name,DNSHostName,DistinguishedName,CanonicalName,Description,Enabled,IPv4Address,LockedOut,OperatingSystem,OperatingSystemServicePack,OperatingSystemVersion,whenCreated
+		}
+	ELSEIF (($ADComp|measure).Count -LT 1)
+		{Write-Host "There aren't any computers by that name, please try again";Start-Sleep -S 1}
+	ELSE {Get-ADComputer -Filter {Name -like $ADComp.Name} -Properties * | Select Name,DNSHostName,DistinguishedName,CanonicalName,Description,Enabled,IPv4Address,LockedOut,OperatingSystem,OperatingSystemServicePack,OperatingSystemVersion,whenCreated}
+}
